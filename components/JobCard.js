f1468602ff31255958f12fe42186f835e139ec6b
fantomx11 <fantomx11@gmail.com>
2026-05-11 15:49:35 -0500

    Prevent event propagation on XACT link click
    Added stopPropagation to XACT link click event.

--- components/JobCard.js
+++ components/JobCard.js
@@ -11,7 +11,7 @@ export const JobCard = ({ est, onOpen }) => {
       </div>
       <div style="font-size:11px; color:#666;">${est.customer} - ${est.description}</div>
       <div class="badges">
-          ${est.xactId && html`<span class="badge badge-manual"><a href="https://www.xactanalysis.com/apps/cxa/detail.jsp?mfn=${est.xactId}" target="_blank">XACT</a></span>`}
+          ${est.xactId && html`<span class="badge badge-manual"><a href="https://www.xactanalysis.com/apps/cxa/detail.jsp?mfn=${est.xactId}" target="_blank"  onClick=${(e) => e.stopPropagation()}>XACT</a></span>`}
           ${est.tasks.needsContact && html`<span class="badge badge-urgent">CONTACT DUE</span>`}
           ${(est.tasks.needsWorkAuth || est.tasks.needsSignedCO) && html`<span class="badge badge-auth">NEED AUTH</span>`}
       </div>
