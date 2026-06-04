07e73db44d3e2ed3cce485dff1517deb199d6fc7
fantomx11 <fantomx11@gmail.com>
2026-06-04 10:20:30 -0500

    .

--- wo-to-calendar.js
+++ wo-to-calendar.js
@@ -1,5 +1,5 @@
 (async (devMode = false) => {
-  const baseUrl = devMode ? "https://cdn.statically.io/gh/fantomx11/pcc-scripts@dev" : "https://fantomx11.github.io/pcc-scripts";
+  const baseUrl = devMode ? "https://fantomx11.github.io/pcc-scripts/dev" : "https://fantomx11.github.io/pcc-scripts";
 
   const App = window.App = window.App || {};
 
